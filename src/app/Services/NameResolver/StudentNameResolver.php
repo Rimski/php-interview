89290eaf882264c02069fa4af7e5a2262386593f
Tanner Ward <hoserehh@gmail.com>
2016-10-29 23:06:52 -0600

    a little refactoring

--- src/app/Services/NameResolver/StudentNameResolver.php
+++ src/app/Services/NameResolver/StudentNameResolver.php
@@ -8,36 +8,43 @@ use \Services\RepositoryInterface;
 // get list of names
 $students = array();
 
-$first_names = array_column($records, $firstName);
 // check for duplicate first names
 $classSize = count($array);
-$duplicateNames = array();
+
     // loop through name list
-    function duplicatFinder($array) {
-        
+    function duplicatFinder($first_names) {
+        $duplicateNames = array();
         for ($x = 0; $x < $classSize; $x++) {
             // if name appears more than once push into duplicate array
-            $slicedArray = 
-            if (array_key_exists($array[i], $array)) {
-                
+            $slicedArray = array_slice($first_names, $x)
+            if (array_key_exists($first_names[$x], $slicedArray)) {
+                array_push($duplicateNames, $first_names[$x]);
             }
         }
+        return($duplicateNames);
     };
-echo($students);
 
-function displayNameAdder($array, $duplicateNames) {
+function displayNameResolver($array, $duplicateNames) {
     $duplicateSize = count($duplicateNames);
+    $displayNames = array();
     for ($x = 0; $x < $classSize; $x++) {
-        for ($y = 0; $y < $duplicateSize; $y++) {
-            if ($array[$x] === $duplicateNames[$y]) {
-                //$displayName = ($firstname + $lastName[1]);
+            if (array_key_exists($array[$x]->$firstName, $duplicateNames)) {
+                array_push($displayNames, ($array[$x]->$firstname + " " + $lastName[1]));
             } else {
-                //$displayName = $firstname;
+                array_push($displayNames, ($array[$x]->$firstname));
             }
-        }
+        
     }
+    return($displayNames)
 }
 
+
+public function nameResolver($array) {
+    $first_names = array_column($students, $firstName);
+    $duplicates = duplicatFinder($first_names);
+    return(displayNameResolver($array, $duplicates))
+    
+}
 // add first name to $displayName + last initial if in duplicates array
 
     // loop through name list
